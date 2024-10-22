15b28954424b32e1d93da82893c0717b3d008353
PritamIT2023 <pritammondal8981@gmail.com>
2024-10-22 23:55:53 +0530

    docs: :memo: add swagger json docs in routes registry
    add swagger json docs in routes registry
    
    Ref: #20

--- service/routes.registry.ts
+++ service/routes.registry.ts
@@ -1,11 +1,100 @@
 const RoutesRegistry = {
   createIssueReport: {
-    name: "Create Issue Github",
+    title: "Create Issue Github",
     url: "noauth/issue/create",
     authRequired: false,
     entityRef: "createIssueReport",
     reqMethod: "post",
-    controllerRef: "createIssueReport"
+    controllerRef: "createIssueReport",
+    swaggerJson: {
+      "tags": [
+        "Support Module"
+      ],
+      "requestBody": {
+          "description": "Add education Info",
+          "content": {
+            "application/json": {
+              "schema": {
+                "type": "object",
+                "properties": {
+                  "title": {
+                    "type": "string"
+                  },
+                  "description": {
+                    "type": "string"
+                  },
+                  "stepsToCreate": {
+                    "type": "string"
+                  },
+                  "stackTrace": {
+                    "type": "string"
+                  },
+                  "devInfo": {
+                    "type": "string"
+                  },
+                  "reporterInfo": {
+                    "type": "string"
+                  },
+                  "labels": {
+                    "type": "string"
+                  }
+                }
+              }
+            }
+          },
+          "required": true
+        },
+        "responses": {
+          "200": {
+            "description": "Successful operation",
+            "content": {
+              "application/json": {
+                "schema": {
+                  "type": "object",
+                  "properties": {
+                    "message": {
+                      "type": "string"
+                    }
+                  }
+                },
+                "examples": {
+                  "CreateIssue": {
+                    "value": {
+                      "message": "Issue create success"
+                    }
+                  }
+                }
+              }
+            }
+          },
+          "404": {
+            "description": "Request API Not Found!!"
+          },
+          
+          "500": {
+            "description": "Internal Server Error",
+            "content": {
+              "application/json": {
+                "schema": {
+                  "type": "object",
+                  "properties": {
+                    "message": {
+                      "type": "string"
+                    }
+                  }
+                },
+                "examples": {
+                  "CreateIssue": {
+                    "value": {
+                      "message": "***"
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+    }
   }
 };
 export default RoutesRegistry;
